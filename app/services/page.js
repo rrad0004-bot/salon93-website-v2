ef1a1430e0b55aea9370e7451d65e7c2e9660738
Rahul Radhakrishnan <rrad0004@student.monash.edu>
2026-05-05 21:48:01 +1000

    service page

---
+++ app/services/page.js
@@ -0,0 +1,21 @@
+export default function ServicesPage() {
+    return (
+        <>
+            <main style={{ padding: "4rem 2rem" }}>
+                <h1>Our Services</h1>
+
+                <p>
+                    At Salon 93, we provide professional women’s hair services including:
+                </p>
+
+                <ul>
+                    <li>Haircuts & Restyling</li>
+                    <li>Hair Colour & Balayage</li>
+                    <li>Blow Waves & Styling</li>
+                    <li>Hair Treatments & Repair</li>
+                    <li>Event & Bridal Styling</li>
+                </ul>
+            </main>
+        </>
+    );
+}
