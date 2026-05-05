d618622da86e78d2629403f86939de5dd5a1c879
Rahul Radhakrishnan <rrad0004@student.monash.edu>
2026-05-05 22:09:54 +1000

    test again

--- app/layout.js
+++ app/layout.js
@@ -1,10 +1,10 @@
 import "./globals.css";
 import Navbar from "../components/Navbar/Navbar";
+
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>
-        <Navbar />
         {children}
       </body>
     </html>
