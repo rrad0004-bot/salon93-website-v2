3f1d26de7a9ae3f83d4d2be9fca8fec8587724cd
Rahul Radhakrishnan <rrad0004@student.monash.edu>
2026-05-07 21:42:50 +1000

    homepage changes

--- app/layout.js
+++ app/layout.js
@@ -4,9 +4,32 @@ import Navbar from "../components/Navbar/Navbar";
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>
+      <head>
+        {/* PREMIUM FONT SYSTEM */}
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&display=swap"
+          rel="stylesheet"
+        />
+      </head>
+
+      <body
+        style={{
+          margin: 0,
+          padding: 0,
+          fontFamily: "Inter, sans-serif",
+          backgroundColor: "#fff",
+          color: "#111",
+          overflowX: "hidden",
+        }}
+      >
         <Navbar />
-        {children}
+
+        {/* PAGE CONTENT WRAPPER */}
+        <main style={{ paddingTop: "80px" }}>
+          {children}
+        </main>
       </body>
     </html>
   );
