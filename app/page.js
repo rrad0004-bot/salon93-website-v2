6eaa464c089e8762ba676b759f6320afdf89e976
Rahul Radhakrishnan <rrad0004@student.monash.edu>
2026-05-07 22:11:18 +1000

    fix

--- app/page.js
+++ app/page.js
@@ -2,8 +2,8 @@
 
 import Link from "next/link";
 import { motion } from "framer-motion";
-import { PrimaryButton, OutlineButton } from "./components/ui/button";
-import { Card } from "./components/ui/card";
+import { PrimaryButton, OutlineButton } from "../components/ui/button";
+import { Card } from "../components/ui/card";
 
 /* ANIMATIONS */
 const fadeUp = {
